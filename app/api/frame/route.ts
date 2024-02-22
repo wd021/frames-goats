7fbfd08143df32de8cee73cc2767b5e2b7bf3fbb
wd021 <dannywitters@gmail.com>
2024-02-22 23:28:21 +0000

    add backup

---
+++ app/api/frame/route.ts
@@ -0,0 +1,44 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
+import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../config';
+import { getRandomKey } from '../../../utils/helpers';
+import nftData from '../../data/nft.json';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const randomKey = getRandomKey(nftData);
+  const randomNft: any = nftData[randomKey];
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          action: 'post',
+          label: '🔄 Random',
+        },
+        {
+          action: 'link',
+          label: '👀 Watch Reel',
+          target: randomNft.video,
+        },
+        {
+          action: 'link',
+          label: 'Mint',
+          target: randomNft.zora,
+        },
+      ],
+      image: {
+        src: 'https://frames.goatcollection.xyz/images/' + randomKey + '.png',
+        aspectRatio: '1:1',
+      },
+      ogDescription: 'The Metaverse’s Hall of Fame 🐐🔥',
+      ogTitle: 'Goat Collection',
+      postUrl: `${NEXT_PUBLIC_URL}/api/random-moment`,
+    }),
+  );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
